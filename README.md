24799aba679cd91866f76b0bae97f83bc36ee612
Adam Burucs <adam.burucs@gmail.com>
2024-08-08 20:36:58 +0200

    init: add files

---
+++ README.md
@@ -0,0 +1,19 @@
+# John Carmack Planner (JCP)
+
+Generate random plan files in John Carmack style.
+
+## Install
+
+[First, you need to install Rust on your system.](https://www.rust-lang.org/tools/install)
+
+## Run
+
+In the project folder, execute: `cargo run`
+
+## License
+
+See [the LICENSE file](LICENSE).
+
+## History
+
+I started the project in August 2024.
---
+++ src/main.rs
@@ -0,0 +1,201 @@
+// Copyright 2024 Adam Burucs. MIT license.
+use rand::prelude::*;
+
+#[derive(PartialEq)]
+enum VerbType {
+    Present,
+    Past,
+}
+
+#[warn(dead_code)]
+struct Noun {
+    singular: &'static str,
+    plural: &'static str,
+}
+
+#[warn(dead_code)]
+struct Verb {
+    present: &'static str,
+    past: &'static str,
+}
+
+const NOUNS: [Noun; 21] = [
+    Noun {
+        singular: "level",
+        plural: "levels",
+    },
+    Noun {
+        singular: "enemy",
+        plural: "enemies",
+    },
+    Noun {
+        singular: "deathmatch",
+        plural: "deathmatches",
+    },
+    Noun {
+        singular: "player movement",
+        plural: "player movements",
+    },
+    Noun {
+        singular: "cheat code",
+        plural: "cheat codes",
+    },
+    Noun {
+        singular: "performance",
+        plural: "performances",
+    },
+    Noun {
+        singular: "buffer",
+        plural: "buffers",
+    },
+    Noun {
+        singular: "demo",
+        plural: "demos",
+    },
+    Noun {
+        singular: "release change",
+        plural: "release changes",
+    },
+    Noun {
+        singular: "server",
+        plural: "servers",
+    },
+    Noun {
+        singular: "network connection",
+        plural: "network connections",
+    },
+    Noun {
+        singular: "monster",
+        plural: "monsters",
+    },
+    Noun {
+        singular: "matrix",
+        plural: "matrices",
+    },
+    Noun {
+        singular: "zooming",
+        plural: "zoomings",
+    },
+    Noun {
+        singular: "scene",
+        plural: "scenes",
+    },
+    Noun {
+        singular: "space partitioning",
+        plural: "space partitionings",
+    },
+    Noun {
+        singular: "surface cache",
+        plural: "surface caches",
+    },
+    Noun {
+        singular: "config file",
+        plural: "config files",
+    },
+    Noun {
+        singular: "rendering",
+        plural: "renderings",
+    },
+    Noun {
+        singular: "texture",
+        plural: "textures",
+    },
+    Noun {
+        singular: "light",
+        plural: "lights",
+    },
+];
+
+const VERBS: [Verb; 8] = [
+    Verb {
+        present: "add",
+        past: "added",
+    },
+    Verb {
+        present: "fix",
+        past: "fixed",
+    },
+    Verb {
+        present: "remove",
+        past: "removed",
+    },
+    Verb {
+        present: "test",
+        past: "tested",
+    },
+    Verb {
+        present: "debug",
+        past: "debugged",
+    },
+    Verb {
+        present: "refactor",
+        past: "refactored",
+    },
+    Verb {
+        present: "design",
+        past: "designed",
+    },
+    Verb {
+        present: "improve",
+        past: "improved",
+    },
+];
+
+const ADJECTIVES: [&str; 17] = [
+    "referential",
+    "close",
+    "far",
+    "top",
+    "bottom",
+    "solid",
+    "opaque",
+    "binary",
+    "hexadecimal",
+    "low-level",
+    "high-level",
+    "broken",
+    "partial",
+    "bright",
+    "dark",
+    "frozen",
+    "stuck",
+];
+
+const MAX_VERBS: usize = VERBS.len();
+const MAX_NOUNS: usize = NOUNS.len();
+const MAX_ADJECTIVES: usize = ADJECTIVES.len();
+
+fn generate_sentence(verb_type: VerbType) -> String {
+    let mut rng = thread_rng();
+    let rand_plural = rng.gen_range(0..1);
+    let rand_verb = rng.gen_range(0..MAX_VERBS);
+    let rand_adjective = rng.gen_range(0..MAX_ADJECTIVES);
+    let rand_noun = rng.gen_range(0..MAX_NOUNS);
+
+    let verb: &str = if verb_type == VerbType::Present {
+        VERBS[rand_verb].present
+    } else {
+        VERBS[rand_verb].past
+    };
+
+    let noun: &str = if rand_plural == 0 {
+        NOUNS[rand_noun].singular
+    } else {
+        NOUNS[rand_noun].plural
+    };
+
+    let sentence = format!("{} {} {}", verb, ADJECTIVES[rand_adjective], noun);
+    sentence
+}
+
+fn main() {
+    println!("John Carmack Planner");
+    println!("Generate random plan files like John Carmack did");
+    println!();
+    println!("Generating plan notes...");
+    println!();
+    for _i in 0..10 {
+        let sentence = generate_sentence(VerbType::Past);
+        println!("{sentence}");
+    }
+}
